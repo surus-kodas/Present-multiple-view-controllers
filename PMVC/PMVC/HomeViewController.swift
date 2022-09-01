3ed0de2694893e21e26fb9ac19c7d17192a6b992
Sūrus Kodas <surus-kodas@git.local>
2022-09-01 21:00:21 +0300

    Add Home View Contreller

---
+++ PMVC/PMVC/HomeViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    let button: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Present child", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .purple
+        setupViews()
+    }
+
+    func setupViews() {
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func buttonAction(_ sender: UIButton) {
+        print("Button tapped!")
+    }
+}
+
